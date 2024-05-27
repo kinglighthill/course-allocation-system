b798fd84b60e49c2eac12e1e4d3a5448063d7ebc
Kingsley Ugwudinso <kingholyhill@gmail.com>
2024-05-28 00:39:50 +0100

    bug fixes

--- routes/admin.mjs
+++ routes/admin.mjs
@@ -1,6 +1,7 @@
 import express from "express";
 import db from "../db/conn.mjs";
 
+import { ObjectId } from "mongodb";
 import { decodeToken } from "../middleware.mjs"
 import { reponseSuccess, reponseError, hashPassword, verifyPassword, isEmailValid, adminExists, lecturerExists, generatePassword } from "../utils.mjs"
 import { COLLECTION_LECTURERS, HOD, LECTURER } from "../constants.mjs";
@@ -97,20 +98,20 @@ router.post("/lecturers", decodeToken, async (req, res) => {
             lecturerNameSet.add(name)
             processedLecturers.push(lecturer)
 
-            processedLecturersMinified.push({
-                "title": lecturer.title,
-                "fullname": lecturer.fullname,
-                "department": lecturer.department,
-                "designation": lecturer.designation,
-                "phone_number": lecturer.phone_number,
-                "email": lecturer.email,
-                "type": lecturer.type,
-                "initial_password": lecturer.initial_password,
-                "updated_password": lecturer.updated_password,
-                "created_at": lecturer.created_at,
-                "updated_at": lecturer.updated_at,
-                "_id": lecturer._id
-            })
+            // processedLecturersMinified.push({
+            //     "title": lecturer.title,
+            //     "fullname": lecturer.fullname,
+            //     "department": lecturer.department,
+            //     "designation": lecturer.designation,
+            //     "phone_number": lecturer.phone_number,
+            //     "email": lecturer.email,
+            //     "type": lecturer.type,
+            //     "initial_password": lecturer.initial_password,
+            //     "updated_password": lecturer.updated_password,
+            //     "created_at": lecturer.created_at,
+            //     "updated_at": lecturer.updated_at,
+            //     "_id": lecturer._id
+            // })
         }
         
         let result = await collection.insertMany(processedLecturers)
@@ -119,7 +120,85 @@ router.post("/lecturers", decodeToken, async (req, res) => {
             return reponseError(res, "failed to add lecturers", 400)
         }
   
-        reponseSuccess(res, "successful", processedLecturersMinified)
+        
+        reponseSuccess(res, "successful", lecturers.map(lecturer => {
+            delete lecturer.password
+            return lecturer
+        }))
+    } catch(error) {
+        console.log("Error: ", error)
+        reponseError(res, "failed to add lecturers")
+    } 
+});
+
+router.get("/lecturers", decodeToken, async (req, res) => {
+    try {
+        const { email } = res.locals.decoded_token
+
+        const adminAvailable = await adminExists(email)
+
+        if (!adminAvailable) {
+            return reponseError(res, "Invalid credentials!", 401)
+        }
+
+        let collection = await db.collection(COLLECTION_LECTURERS)
+        
+        let result = await collection.find()
+
+        if (!result.acknowledged) {
+            return reponseError(res, "failed to add lecturers", 400)
+        }
+  
+        reponseSuccess(res, "successful", result)
+    } catch(error) {
+        console.log("Error: ", error)
+        reponseError(res, "failed to add lecturers")
+    } 
+});
+
+router.get("/lecturers/:id", decodeToken, async (req, res) => {
+    try {
+        const { email } = res.locals.decoded_token
+
+        const adminAvailable = await adminExists(email)
+
+        if (!adminAvailable) {
+            return reponseError(res, "Invalid credentials!", 401)
+        }
+
+        const id = req.params.id
+        const objectId = new ObjectId(id)
+
+        let collection = await db.collection(COLLECTION_LECTURERS)
+        
+        let result = await collection.find({_id: objectId})
+
+        if (!result.acknowledged) {
+            return reponseError(res, "failed to add lecturers", 400)
+        }
+  
+        reponseSuccess(res, "successful", result)
+    } catch(error) {
+        console.log("Error: ", error)
+        reponseError(res, "failed to add lecturers")
+    } 
+});
+
+router.put("/lecturers/update", decodeToken, async (req, res) => {
+    try {
+        const { email } = res.locals.decoded_token
+
+        const adminAvailable = await adminExists(email)
+
+        if (!adminAvailable) {
+            return reponseError(res, "Invalid credentials!", 401)
+        }
+
+        let result = await collection.find()
+
+  
+        
+        reponseSuccess(res, "successful", result)
     } catch(error) {
         console.log("Error: ", error)
         reponseError(res, "failed to add lecturers")
